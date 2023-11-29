4f38a9cef36ad7a98d4c3fb27f38191da54fe457
Riyumimo <ilhamsuherman80@gmail.com>
2023-11-29 11:19:04 +0700

    ui update change checkout screen

--- lib/presentation/payment/check_out_screen.dart
+++ lib/presentation/payment/check_out_screen.dart
@@ -1,72 +1,147 @@
 import 'package:flutter/material.dart';
 
-class CheckoutScreen extends StatelessWidget {
-  const CheckoutScreen({super.key});
+class CheckOutScreen extends StatelessWidget {
+  const CheckOutScreen({super.key});
   static const name = "/checkout";
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Container(
-          height: 15,
-          width: 122,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: const Color(0xffA8A8A8),
-          ),
-        ),
-        centerTitle: false,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
+        title: const Text("Checkout"),
+        centerTitle: true,
       ),
-      body: ListView.separated(
-        itemBuilder: (context, index) => Container(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Row(
+      body: SingleChildScrollView(
+        // physics: veri,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: Colors.white,
+                // shadowColor: Colors.white,
+                surfaceTintColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: const Icon(Icons.location_on),
+                      title: const Text("Lokasi Pengiriman"),
+                      trailing: IconButton(
+                          onPressed: () {}, icon: const Icon(Icons.edit)),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 50),
+                      child: Text(
+                          "Lorem ipsum, atau ringkasnya lipsum, adalah teks standar yang ditempatkan untuk mendemostrasikan elemen grafis atau presentasi visual seperti font,"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+            Column(
+              children: List.generate(3, (index) => const CheckOutItem()),
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+            InkWell(
+              onTap: () {
+                print("payment");
+              },
+              child: const SizedBox(
+                height: 30,
+                child: Row(
+                  children: [
+                    Text("Payment Method"),
+                    Spacer(),
+                    Text("Qris"),
+                    Icon(Icons.arrow_forward_ios_rounded)
+                  ],
+                ),
+              ),
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text("SubTotal:"), Text("30.000")],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text("Ongkos Pengiriman:"), Text("30.000")],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text("Discount:"), Text("30.000")],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text("Fee Aplikasi"), Text("3.000")],
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text("Total"), Text("30.000")],
+            ),
+            OutlinedButton(
+                onPressed: () {}, child: const Text("Lakukan Pembayaran"))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CheckOutItem extends StatelessWidget {
+  const CheckOutItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Row(
+        children: [
+          Container(
+            height: 80,
+            width: 80,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle, color: Color(0xffC4C4C4)),
+          ),
+          const SizedBox(
+            width: 16.0,
+          ),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                height: 80,
-                width: 80,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle, color: Color(0xffC4C4C4)),
+              SizedBox(
+                height: 20,
+                width: 183,
+                child: Text("Procuct 1"),
               ),
-              const SizedBox(
-                width: 16.0,
+              SizedBox(
+                height: 12,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Text("Rp.10.000.000"),
+              Row(
                 children: [
-                  Container(
-                    height: 20,
-                    width: 183,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: const Color(0xffA8A8A8),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Container(
-                    height: 15,
-                    width: 244,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: const Color(0xffD0D0D0),
-                    ),
-                  ),
+                  Text("0"),
                 ],
-              )
+              ),
             ],
           ),
-        ),
-        separatorBuilder: (context, index) => const Divider(
-          color: Color(0xffD0D0D0),
-          thickness: 1,
-        ),
-        itemCount: 4,
+        ],
       ),
     );
   }
